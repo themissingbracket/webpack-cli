ec90251e9ca0f6f1d013bb409e36587816b0f035
Rahul <k.dasgupta112@gmail.com>
2020-04-13 17:51:05 +0800

    changed the workflow

--- src/Utils/executeWorkFlow.ts
+++ src/Utils/executeWorkFlow.ts
@@ -2,7 +2,7 @@
 
 
 export async function executeWorkflow<T>(workflow:Array<(args:T)=>Promise<void>>, workflowArgument:T):Promise<void> {
-	
+
 	for (const task of workflow) {
 		await task(workflowArgument);
 	}
